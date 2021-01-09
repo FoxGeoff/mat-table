da53ed16e12253aed28e4904d7b5c10eab6d2178
Geoff Fox <fox.geoffrey@gmail.com>
2021-01-08 16:55:04 -0800

    Add supervisor data

--- src/app/Shared/supervisor-data.ts
+++ src/app/Shared/supervisor-data.ts
@@ -1,18 +1,14 @@
-import { Employee } from "./interfaces/employee";
+import { Supervisor } from "./interfaces/supervisor";
 
 export class SupervisorData {
 
-  private supervisors: Employee[] = [
-    { id: 1, companyId: 1, firstName: 'Arvilla', lastName: 'Hazel', email: 'ah@gmail.com', phone: '858-666-123', isActive: true },
-    { id: 2, companyId: 1, firstName: 'Demetrius', lastName: 'Johnson', email: 'dj@gmail.com', phone: '858-676-123', isActive: true },
-    { id: 3, companyId: 1, firstName: 'Brian', lastName: 'Hemenway', email: 'bh@gmail.com', phone: '858-567-700', isActive: true },
-    { id: 4, companyId: 1, firstName: 'Elise', lastName: 'Toohey', email: 'et@gmail.com', phone: '858-245-687', isActive: true },
-    { id: 5, companyId: 1, firstName: 'Marcella', lastName: 'Ryberg', email: 'mr@gmail.com', phone: '858-829-122', isActive: true },
-    { id: 6, companyId: 1, firstName: 'Joe', lastName: 'Conyers', email: 'jj@gmail.com', phone: '858-164-153', isActive: true },
+  private supervisors: Supervisor[] = [
+    { id: 1, companyId: 1, firstName: 'Joanna', lastName: 'Fox', email: 'joe@gmail.com', phone: '858-666-321', isActive: true },
+    { id: 2, companyId: 1, firstName: 'Geoff', lastName: 'Fox', email: 'geoff@gmail.com', phone: '858-676-321', isActive: true },
   ];
 
-  getAllEmployees(): Employee[] {
-    return this.employees.slice();
+  getAllSupervisors(): Supervisor[] {
+    return this.supervisors.slice();
   }
 
 }
